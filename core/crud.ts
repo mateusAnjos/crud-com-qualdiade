3099752e0f897cc020c0461dea886253856a6615
mateusAnjos <mateus.anjosdefaria@gmail.com>
2024-07-09 01:43:45 -0300

    First commit

---
+++ core/crud.ts
@@ -0,0 +1,49 @@
+import fs from "fs"
+// const fs = require("fs") - CommonJS
+const DB_FILE_PATH = "./core/db"
+
+console.log("[CRUD]");
+
+interface Todo {
+    date:string;
+    content:string;
+    done:boolean;
+}
+
+function create(content: string) {
+    const todo: Todo = {
+      date: new Date().toISOString(),
+      content: content,
+      done: false,
+    };
+  
+    const todos: Array<Todo> = [
+      ...read(),
+      todo,
+    ];
+
+//salvar conteudo no sistema
+fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos
+}, null, 2));
+return content
+}
+
+function read():Array<Todo>{
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8")
+    const db =JSON.parse(dbString || "{}")
+    if(!db.todos){
+        return []
+    }
+    return db.todos
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "");
+  }
+  
+  // [SIMULATION]
+  CLEAR_DB()
+create("Primeira TODO")
+create("Segunda TODO")
+console.log(read())
